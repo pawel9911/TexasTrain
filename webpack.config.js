52ec970095c96cb4cc6b5699fddde91acba6cd72
Paweł Grzybek <pawelgrzybek2@wp.pl>
2020-09-25 15:10:50 +0200

    Configuration

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const entryPath = "js";
+const entryFile = "app.js";
+
+module.exports = {
+  entry: `./${entryPath}/${entryFile}`,
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, `${entryPath}/build`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `${entryPath}`),
+    publicPath: "/build/",
+    compress: true,
+    port: 3001
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
+};
