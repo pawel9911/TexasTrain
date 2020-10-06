beed1140b9e2557f87fce3d83ed3a693095884f5
Paweł Grzybek <pawelgrzybek2@wp.pl>
2020-10-06 23:24:10 +0200

    done config, add trains with animation, few options

--- js/app.js
+++ js/app.js
@@ -1,24 +1,75 @@
 import '../scss/main.scss';
-// import '../img';
+// import ddd from "../img/westernTown.jpg"
 
 document.addEventListener('DOMContentLoaded', function(){
-    const backgroundImg = document.querySelector('.mainBox');
-    backgroundImg.style.backgroundImage = 'url("../img/westernTown.jpg")';
-
-    const bigTrains = document.querySelectorAll('.bigTrain');
-    bigTrains.forEach((train)=> train.style.backgroundImage = 'url("../img/bigTrain.jpg")')
-
-    const smallTrains = document.querySelectorAll('.smallTrain');
-    smallTrains.forEach((train)=> train.style.backgroundImage = 'url("../img/smallTrain.jpg")')
+    // const backgroundImg = document.querySelector('.mainBox');
+    // backgroundImg.style.backgroundImage = `url(${ddd})`;
  
     const shadowBox = document.querySelector(".shadowBox");
     const buttonStart = shadowBox.querySelector("button");
     const gameBox = document.querySelector(".gameBox")
+
+    const optionsTrain = [document.querySelector('.smallTrain'), document.querySelector('.bigTrain')];
+
+
+
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var x = canvas.width/2;
+    var y = canvas.height-58;
+    var dy = -2;
+    const jump  = () =>{
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    const clearJump = () =>{
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        jump();
+        y += dy;
+    }
+    const falldown = () =>{
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        jump();
+        y -= dy;
+    }
+    canvas.addEventListener('mouseover', clearJump);
+    canvas.addEventListener('mouseout', falldown);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
     buttonStart.addEventListener("click", function(){
         shadowBox.style.display = "none";
         this.style.display = "none";
         gameBox.style.display = "flex";
 
+        // let drawTrain = setInterval(() => {
+        //     const indexTrain = Math.floor(Math.random()*2)
+        //     const nextTrain = optionsTrain[indexTrain].cloneNode();
+        //     console.log(gameBox.firstElementChild);
+        //     gameBox.appendChild(nextTrain);
+        //     gameBox.firstElementChild.parentElement.removeChild(gameBox.firstElementChild);
+        // }, 4000);
+
     })
+
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,67 @@
 const path = require("path");
-const entryFile = "app.js";
+const autoprefixer = require('autoprefixer');
+const Html = require('html-webpack-plugin');
+const MiniCSS = require("mini-css-extract-plugin");
+const Compression = require("compression-webpack-plugin");
 
+const entryFile = "app.js";
 module.exports = {
   entry: `./js/${entryFile}`,
   output: {
     filename: "out.js",
-    path: path.resolve(__dirname, `build`)
+    path: path.resolve(__dirname, `./build`)
   },
   devServer: {
-    contentBase: path.join(__dirname, `/`),
+    contentBase: path.join(__dirname),
     publicPath: "/build/",
     compress: true,
-    port: 3000
+    port: 3001
   },
+  mode: "development",
+  devtool: "source-map",
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
       },
       {
         test: /\.scss$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif|jpg)$/i,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
         use: [
+          'style-loader',
           {
-            loader: 'url-loader',
+            loader: 'css-loader',
             options: {
-              limit: 8192,
-            },
+              sourceMap: true
+            }
           },
-        ],
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       },
-      
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
+        loader: "file-loader",
+        options: {
+          name: "[name].[ext]",
+          publicPath: "/img/",
+          outputPath: "/img/"
+        }
+      }
     ]
-  }
+  },
+  plugins: [
+    new Html({
+      filename: "index.html",
+      template: `./index.html`
+    }),
+    new Compression({
+      threshold: 0,
+      minRatio: 0.8
+    })
+  ]
 };
