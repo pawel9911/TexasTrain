bf4197455091e4d96f0483c3cdb04fb97eed6432
Paweł Grzybek <pawelgrzybek2@wp.pl>
2020-10-09 18:57:29 +0200

    add train and remove, but with bug

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import '../scss/main.scss';
-// import ddd from "../img/westernTown.jpg"
+// import ddd from "../img/westernTown.jpg";
+// import bigImg from '../img/bigTrain.jpg';
 
 document.addEventListener('DOMContentLoaded', function(){
     // const backgroundImg = document.querySelector('.mainBox');
@@ -7,86 +8,55 @@ document.addEventListener('DOMContentLoaded', function(){
  
     const shadowBox = document.querySelector(".shadowBox");
     const buttonStart = shadowBox.querySelector("button");
-    const gameBox = document.querySelector(".gameBox")
-
+    const gameBox = document.querySelector(".gameBox");
+    const trainBox = document.querySelector('.train');
+    const cowboy = document.querySelector('.cowboy');
     const optionsTrain = [document.querySelector('.smallTrain'), document.querySelector('.bigTrain')];
+    const allTrain = [...document.querySelector('.train').children];
+    let leftChange = 0;
 
+    const positionCowboyStartX = 113;
+    let positionCowboyStartY = 157;
 
-
-    const canvas = document.getElementById("myCanvas");
-    const ctx = canvas.getContext("2d");
-    
-    let y = canvas.height-58;
-    
-    let dy = -2;
-    const ballRadius = 10;
-    let press = false;
-
-    document.addEventListener("keydown", keyDownHandler);
-    document.addEventListener("keyup", keyUpHandler);
 
     function keyDownHandler(e) {
-        if(e.keyCode == 32){
-            press = true;
+        if((e.keyCode == 32)&&(positionCowboyStartY>90)){//warunek zeby uciekało tylko do danej odleglosci
+            positionCowboyStartY -= 70;
+            cowboy.style.top = `${positionCowboyStartY}px`;
         }
     }
     function keyUpHandler(e) {
         if(e.keyCode == 32) {
-            press = false;
+            positionCowboyStartY = 157;
+            cowboy.style.top = `${positionCowboyStartY}px`;
         }
     }
-
-    const jump  = () =>{
-        ctx.beginPath();
-        ctx.arc(133, y, ballRadius, 0, Math.PI*2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-    }
-    const clearJump = () =>{
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        jump();
-        if((press == true) && (y >= 93)){
-            y += dy;
-        }
-        else if((press == false) && (y <194)){
-            y -= dy
-        }
-        // if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-        //     dy = -dy;
-        // }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     buttonStart.addEventListener("click", function(){
         shadowBox.style.display = "none";
-        this.style.display = "none";
         gameBox.style.display = "flex";
-        setInterval(clearJump, 10)
-        // let drawTrain = setInterval(() => {
-        //     const indexTrain = Math.floor(Math.random()*2)
-        //     const nextTrain = optionsTrain[indexTrain].cloneNode();
-        //     console.log(gameBox.firstElementChild);
-        //     gameBox.appendChild(nextTrain);
-        //     gameBox.firstElementChild.parentElement.removeChild(gameBox.firstElementChild);
-        // }, 4000);
+
+        const addTrain = setInterval(() => {
+            const indexTrain = Math.floor(Math.random()*2)
+            const nextTrain = optionsTrain[indexTrain].cloneNode();
+            trainBox.appendChild(nextTrain);
+            allTrain.push(nextTrain);
+            trainBox.firstElementChild.parentElement.removeChild(trainBox.firstElementChild);
+        }, 4000);
+
+        const moveTrain = setInterval(() => {
+            allTrain.forEach((tr)=>{
+                tr.style.left = `${leftChange}px`;
+            })
+            if(leftChange>=-250){
+                leftChange-=0.625;
+            }
+            else{leftChange = 0}
+        }, 10)
+
+        document.addEventListener("keydown", keyDownHandler);
+        document.addEventListener("keyup", keyUpHandler);
+
 
     })
 
