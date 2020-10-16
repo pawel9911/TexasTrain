ea7f36ff58226d5e00a124d702e5d58cf93d6f21
Paweł Grzybek <pawelgrzybek2@wp.pl>
2020-10-16 18:39:16 +0200

    money and good move function

--- js/app.js
+++ js/app.js
@@ -7,9 +7,10 @@ document.addEventListener('DOMContentLoaded', function(){
  
     const shadowBox = document.querySelector(".shadowBox");
     const gameBox = document.querySelector(".gameBox");
-    const trainBox = document.querySelector('.trainBox');
+    // const trainBox = document.querySelector('.trainBox');
     const buttonStart = shadowBox.querySelector("button");
     const cowboy = document.querySelector('.cowboy');
+    const money = document.querySelector('.money')
 
     const optionsTrain = [document.querySelector('.smallTrain'), document.querySelector('.bigTrain')];
     let allTrain = [...document.querySelector('.trainBox').children];
@@ -23,8 +24,9 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let press = true;
     
-    let timeAddTrain = 4000;
     let timeMoveTrain = 10;
+
+   
     
     function keyDownHandler(e) {
         if ((e.keyCode == 32)&&(press === true)){
@@ -61,7 +63,7 @@ document.addEventListener('DOMContentLoaded', function(){
         }
         press = false;
     }
-    
+
     function keyUpHandler(e) {
         //1
         if((e.keyCode == 32)&&(press === false)&&(allTrain[0].className === 'smallTrain')&&(allTrain[1].className === 'smallTrain')){
@@ -105,44 +107,32 @@ document.addEventListener('DOMContentLoaded', function(){
         shadowBox.style.display = "none";
         gameBox.style.display = "flex";
 
-        const addTrain = setInterval(() => {
-            const indexTrain = Math.floor(Math.random()*2)
-            const nextTrain = optionsTrain[indexTrain].cloneNode();
-            // trainBox.appendChild(nextTrain);
-            // allTrain.push(nextTrain);
-            // allTrain.shift();
-            allTrain = [...allTrain, nextTrain];
-            allTrain.shift();
-            // trainBox.firstElementChild.parentElement.removeChild(trainBox.firstElementChild);
+        const moveTrain = setInterval(() => {
             const tra = document.querySelector('.trainBox');
             tra.innerHTML = ''
-            allTrain.forEach(el => {
-            tra.appendChild(el)
-         });
-
-            //punkty i przyspieszenie
-            score++;
-            counterScore.innerText = `Score: ${score}`;
-
-        }, timeAddTrain);
-
-        const moveTrain = setInterval(() => {
             allTrain.forEach((tr)=>{
                 tr.style.left = `-${leftChange}px`;
+                tra.appendChild(tr)
             })
             if(leftChange<250){
                 leftChange+=0.625;
              }
             else{
-                leftChange = 0
+                leftChange = 0;
+                const indexTrain = Math.floor(Math.random()*2)
+                const nextTrain = optionsTrain[indexTrain].cloneNode();
+                allTrain = [...allTrain, nextTrain];
+                allTrain.shift();
+
+                //punkty i przyspieszenie
+                score++;
+                counterScore.innerText = `Score: ${score}`;
             }
             if((`${-positionCowboyStartX-0.125}px` === allTrain[1].style.left)&&(positionCowboyStartY>90)&&(allTrain[1].className === 'bigTrain')){
                 clearInterval(moveTrain);
-                clearInterval(addTrain);
                 leftChange = 0;
                 score = 0;
                 timeMoveTrain = 10;
-                timeAddTrain = 4000;
                 counterScore.innerText = `Score: ${score}`
                 shadowBox.style.display = "flex";
                 gameBox.style.display = "none";
@@ -152,6 +142,17 @@ document.addEventListener('DOMContentLoaded', function(){
                 positionCowboyStartY = 130;
                 cowboy.style.top = `${positionCowboyStartY}px`;
             }
+
+            //Moneta
+            let xMoney = money.getBoundingClientRect().x;
+            let yMoney = money.getBoundingClientRect().y;
+            let xCowboy = cowboy.getBoundingClientRect().x;
+            let yCowboy = cowboy.getBoundingClientRect().y;
+            if ((xMoney>=xCowboy)&&(xMoney<xCowboy+cowboy.getBoundingClientRect().width)&&(yCowboy<=yMoney)){
+                money.style.display = 'none';
+                score+=5;
+            }
+            console.log(cowboy.getBoundingClientRect().y, money.getBoundingClientRect().y)
         }, timeMoveTrain)
 
         document.addEventListener("keydown", keyDownHandler);
