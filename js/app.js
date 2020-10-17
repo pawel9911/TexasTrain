55535a512cfeebdcd87e960013b91b5309882cb9
Paweł Grzybek <pawelgrzybek2@wp.pl>
2020-10-17 15:18:44 +0200

    without speed

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
 import '../scss/main.scss';
-// import ddd from "../img/westernTown.jpg";
 
 document.addEventListener('DOMContentLoaded', function(){
-    // const backgroundImg = document.querySelector('.mainBox');
-    // backgroundImg.style.backgroundImage = `url(${ddd})`;
- 
     const shadowBox = document.querySelector(".shadowBox");
     const gameBox = document.querySelector(".gameBox");
-    // const trainBox = document.querySelector('.trainBox');
     const buttonStart = shadowBox.querySelector("button");
     const cowboy = document.querySelector('.cowboy');
-    const money = document.querySelector('.money')
+    const money = document.querySelector('.money');
 
     const optionsTrain = [document.querySelector('.smallTrain'), document.querySelector('.bigTrain')];
     let allTrain = [...document.querySelector('.trainBox').children];
@@ -24,14 +19,10 @@ document.addEventListener('DOMContentLoaded', function(){
 
     let press = true;
     
-    let timeMoveTrain = 10;
-
-   
-    
     function keyDownHandler(e) {
         if ((e.keyCode == 32)&&(press === true)){
             positionCowboyStartY -= 110;
-            cowboy.style.top = `${positionCowboyStartY}px`
+            cowboy.style.top = `${positionCowboyStartY}px`;
         }
         else if((e.keyCode == 32)&&(press === false)&&(allTrain[0].className === 'smallTrain')&&(allTrain[1].className === 'smallTrain')){
             positionCowboyStartY = 130;
@@ -109,36 +100,42 @@ document.addEventListener('DOMContentLoaded', function(){
 
         const moveTrain = setInterval(() => {
             const tra = document.querySelector('.trainBox');
-            tra.innerHTML = ''
+            tra.innerHTML = '';
             allTrain.forEach((tr)=>{
                 tr.style.left = `-${leftChange}px`;
-                tra.appendChild(tr)
+                tra.appendChild(tr);
             })
+
+            //Poruszanie
             if(leftChange<250){
                 leftChange+=0.625;
-             }
+            }
             else{
                 leftChange = 0;
-                const indexTrain = Math.floor(Math.random()*2)
+                const indexTrain = Math.floor(Math.random()*2);
                 const nextTrain = optionsTrain[indexTrain].cloneNode();
                 allTrain = [...allTrain, nextTrain];
                 allTrain.shift();
 
-                //punkty i przyspieszenie
+                //Punkty
                 score++;
                 counterScore.innerText = `Score: ${score}`;
             }
+
+            //Kolizja
             if((`${-positionCowboyStartX-0.125}px` === allTrain[1].style.left)&&(positionCowboyStartY>90)&&(allTrain[1].className === 'bigTrain')){
                 clearInterval(moveTrain);
                 leftChange = 0;
                 score = 0;
-                timeMoveTrain = 10;
-                counterScore.innerText = `Score: ${score}`
+                counterScore.innerText = `Score: ${score}`;
                 shadowBox.style.display = "flex";
                 gameBox.style.display = "none";
                 alert('Game Over');
             }
+
+            //Upadek
             else if ((allTrain[0].className === 'bigTrain')&&(allTrain[1].className === 'smallTrain')&&(allTrain[0].style.left === `${-positionCowboyStartX-25.125}px`)){
+                console.log(allTrain[0].style.left)
                 positionCowboyStartY = 130;
                 cowboy.style.top = `${positionCowboyStartY}px`;
             }
@@ -151,9 +148,10 @@ document.addEventListener('DOMContentLoaded', function(){
             if ((xMoney>=xCowboy)&&(xMoney<xCowboy+cowboy.getBoundingClientRect().width)&&(yCowboy<=yMoney)){
                 money.style.display = 'none';
                 score+=5;
+                counterScore.innerText = `Score: ${score}`;
             }
             console.log(cowboy.getBoundingClientRect().y, money.getBoundingClientRect().y)
-        }, timeMoveTrain)
+        }, 10)
 
         document.addEventListener("keydown", keyDownHandler);
         document.addEventListener("keyup", keyUpHandler);    
